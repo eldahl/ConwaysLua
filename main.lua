bee9000bd296af67acb1ef3d423fcbc51de05441
Esben <71466904+ebberx@users.noreply.github.com>
2024-01-10 13:25:01 +0100

    Fixed Love2D update changes and added gif of glider guns

--- main.lua
+++ main.lua
@@ -35,10 +35,13 @@ function love.load()
 	love.filesystem.setIdentity("Conway's Game of Life")			-- Identity for system
 	love.keyboard.setKeyRepeat(true)
 
-	love.graphics.setBackgroundColor(104, 136, 248)					-- Set the background color to a nice blue
+	love.graphics.setBackgroundColor(104, 136, 248, 255)					-- Set the background color to a nice blue
 
 	love.window.setTitle("Conway's Game of Life")					-- Window title
-	love.window.setMode(1024, 768)									-- Game window resolution
+	local flags = {
+		vsync = 0,
+	}
+	love.window.setMode(648, 648, flags)									-- Game window resolution
 
 	gfx.tile = love.graphics.newImage("tile.png")					-- Load textures
 	gfx.black = love.graphics.newImage("black.png")
@@ -65,7 +68,7 @@ function love.load()
 	cells[6][8]:setAlive(true)
 	]]
 
-	-- Glider gun
+	-- Glider gun #1
 	cells[10][12]:setAlive(true)
 	cells[10][13]:setAlive(true)
 
@@ -115,16 +118,78 @@ function love.load()
 	cells[45][10]:setAlive(true)
 	cells[45][11]:setAlive(true)
 
+	-- Glider gun #2
+	cells[10][42]:setAlive(true)
+	cells[10][43]:setAlive(true)
+
+	cells[11][42]:setAlive(true)
+	cells[11][43]:setAlive(true)
+
+	cells[20][42]:setAlive(true)
+	cells[20][43]:setAlive(true)
+	cells[20][44]:setAlive(true)
+
+	cells[21][41]:setAlive(true)
+	cells[21][45]:setAlive(true)
+
+	cells[22][40]:setAlive(true)
+	cells[22][46]:setAlive(true)
+	cells[23][40]:setAlive(true)
+	cells[23][46]:setAlive(true)
+
+	cells[24][43]:setAlive(true)
+
+	cells[25][41]:setAlive(true)
+	cells[25][45]:setAlive(true)
+
+	cells[26][42]:setAlive(true)
+	cells[26][43]:setAlive(true)
+	cells[26][44]:setAlive(true)
+
+	cells[27][43]:setAlive(true)
+
+	cells[30][40]:setAlive(true)
+	cells[30][41]:setAlive(true)
+	cells[30][42]:setAlive(true)
+	cells[31][40]:setAlive(true)
+	cells[31][41]:setAlive(true)
+	cells[31][42]:setAlive(true)
+
+	cells[32][39]:setAlive(true)
+	cells[32][43]:setAlive(true)
+
+	cells[34][38]:setAlive(true)
+	cells[34][39]:setAlive(true)
+	cells[34][43]:setAlive(true)
+	cells[34][44]:setAlive(true)
+
+	cells[44][40]:setAlive(true)
+	cells[44][41]:setAlive(true)
+	cells[45][40]:setAlive(true)
+	cells[45][41]:setAlive(true)
 end
 
 function love.keypressed(key)
-	if key == "up" 		then newDirection = 1 end
+	if key == "space" 	then doUpdate = not doUpdate end
 	if key == "escape" 	then love.event.quit() end
 	if key == "d" 		then update() end
 end
 
-function love.update()
-	update()
+local fps = 60
+local frameTime = 1/fps
+
+function love.update(dt)
+	local startTime = love.timer.getTime()
+	if(doUpdate) then
+		update()
+		local updateTime = love.timer.getTime() - startTime
+	
+		if frameTime > updateTime then
+			-- 0.001 seconds delay is added by the love engine, so we remove that to hit the desired fps
+			local remainingDeltaTime = frameTime - updateTime - 0.001
+			love.timer.sleep(remainingDeltaTime)
+		end
+	end
 end
 
 function update()
@@ -233,16 +298,15 @@ function drawCells()
 	love.graphics.setColor(255,255,255,255)
 	for i = 0, gridSizeX do
 	for j = 0, gridSizeY do
-		love.graphics.draw(cells[i][j].gfx, 14+i*8, 14+j*8, 0, 0.25, 0.25)
+		love.graphics.draw(cells[i][j].gfx, i*8, j*8, 0, 0.25, 0.25)
 	end
 	end
 
 end
 
 function love.draw(dt)
+	love.graphics.clear(love.graphics.getBackgroundColor())
 	drawCells()
-
-	love.graphics.print("x: ")
-	love.graphics.print("y: ", 0, 10)
+	love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 10)
 end
 
